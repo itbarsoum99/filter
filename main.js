398cab13524813f37c6792b1591900764f90ea06
itbarsoum99 <itbarsoum99@gmail.com>
2023-05-24 17:10:18 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+
+let letter = document.getElementById('letter');
+const onMouseMove = (e) =>{
+  letter.style.left = e.pageX + 'px';
+  letter.style.top = e.pageY + 'px';
+}
+document.addEventListener('mousemove', onMouseMove);
+
+
+
+function generateRandomLetter() {
+  letter.classList.remove('fade');
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  letter.innerHTML = randomCharacter;
+  const colors = ["purple", "blue", "green", "red", "orange"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  letter.style.color = randomColor;
+
+  setTimeout(function() {
+    letter.classList.add('fade');
+  }, 300);
+  
+}
+
+
+    
